6993562c4989e5c21902ce3c382d56d13141e5a7
Biplob kafle <biplobkafle21@gmail.com>
2024-07-18 14:54:57 +0545

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // -r dotenv/config --experimental-json-modules                   !!!!!!!!!!!!!!!important!!!!!
+
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-
-
-
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+      app.on("error", (error) => {
+        console.log("ERROR OCCURED", error);
+      });
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB ERROR!!!", error);
+  });
