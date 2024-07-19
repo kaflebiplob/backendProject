943140d42ba5e3a01254a67c47a18b04b4a53b6f
Biplob kafle <biplobkafle21@gmail.com>
2024-07-19 19:17:39 +0545

    issues fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,56 +2,58 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import { uploadOnCloudinary } from "../utils/coludinary.js";
 
 const userRegister = asyncHandler(async (req, res) => {
-  //   res.status(200).json({
-  //     message: "ok",
-  //   });
+  // res.status(200).json({
+  //   message: "ok",
+  // });
 
-  const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+  const { fullname, email, username, password } = req.body;
+  //  console.log("", email, username, password, fullName);
 
   if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "all feilds are required");
   }
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
-    throw ApiError(409, "User already exist");
+    throw new ApiError(409, "User already exist");
   }
-  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar File is Required");
-  }
-  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  // if (!avatarLocalPath) {
+  //   throw new ApiError(400, "Avatar File is Required");
+  // }
+  // const avatar = await  uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-  if (!avatar) {
-    throw new ApiError(400, "Avatar File is Required");
-  }
+  // if (!avatar) {
+  //   throw new ApiError(400, "Avatar File is Required");
+  // }
 
-  User.create({
-    fullName,
-    avatar: avatar.url,
+  const user = await User.create({
+    fullname,
+    // avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
     password,
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findByIdconst(user._id).select(
-    "-password-refreshToken"
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
   );
+
   if (!createdUser) {
     throw new ApiError(500, "somenthing went wrong");
   }
   return res
     .status(201)
-    .json(new ApiResponse(), (200, createdUser, "User registered succesfully"));
+    .json(new ApiResponse(200,createdUser, "User registered succesfully"));
 });
 
 export default userRegister;
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,10 +1,10 @@
 class ApiResponse {
   constructor(statusCode, data, message = "Success") {
-    (this.statusCode = statusCode),
-      (this.data = data),
-      (this.message = message);
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
     this.Success = statusCode < 400;
   }
 }
 
-export{ApiResponse}
+export { ApiResponse };
